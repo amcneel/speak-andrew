d65473269f0d0e4a07b48811f34ce9fa9f455309
Andrew McNeel <amcneel@wustl.edu>
2018-12-25 02:20:46 +0100

    fixed stuff lol

--- public/main.js
+++ public/main.js
@@ -1,6 +1,4 @@
 
-// andrewspeak.surge.sh
-
 const state = 0
 const form = document.getElementById('mainform')
 const button = document.getElementById('voiceBtn')
@@ -29,23 +27,6 @@ const triggerVoice = async (e) => {
     button.disabled = false
   })
   .catch(err => console.log("error: ", err))
-  // console.log('create: ', create)
-}
-
-const play = async () => {
-  const response = await fetch('https://avatar.lyrebird.ai/api/v0/generated', {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer oauth_1EobpX99c1u912Deoudkl2j9gS8",
-      "Content-Type": "application/json"
-    },
-  })
-
-  const data = await response.json()
-  const audio = data.results[0]
-  let a = new Audio(audio.url)
-  a.play()
-  button.disabled = false
 }
 
 form.addEventListener('submit', triggerVoice, false)
